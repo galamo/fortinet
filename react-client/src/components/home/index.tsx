63f69c7ac0ba85b1e7cdbc92217d525a732fc380
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2023-01-22 08:34:10 +0200

    class-

--- react-client/src/components/home/index.tsx
+++ react-client/src/components/home/index.tsx
@@ -2,7 +2,41 @@ import { useCallback, useState } from "react"
 // @ts-ignore
 import { Button } from "primereact/button"
 import { InputText } from "primereact/inputtext"
+import styled from "styled-components";
 
-export function Home() {
-    return <h1> Home </h1>
-}
+
+const SignupForm = ({ className }: any) => (
+    <form className={className}>
+        <input className="input" />
+        <button className="button">Button</button>
+    </form>
+);
+
+const Form = styled(SignupForm)`
+    .input {
+      background-color: palegreen;
+    }
+  
+    .button {
+      background-color: palevioletred;
+    }
+  `;
+
+const Component = ({ className }: any) => {
+    return <div {...{ className }}>Text</div>;
+};
+
+const RedText = styled(Component)`
+    color: red;
+  `;
+
+const Home = () => {
+    return (
+        <>
+            <Form />
+            <RedText />
+        </>
+    );
+};
+
+export { Home };
